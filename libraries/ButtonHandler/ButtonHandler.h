72ab91d68de0e32102f21b22a7c9d538ca1dc7e2
p1234321 <paulpritz@gmail.com>
2016-03-30 15:27:13 +0200

    Added files via upload

---
+++ libraries/ButtonHandler/ButtonHandler.h
@@ -0,0 +1,72 @@
+/* Buttonhandler */
+
+/*
+ Library by Alex Knol (https://github.com/Elexy)
+ */
+
+#ifndef ButtonHandler_h
+#define Buttonhandler_h
+
+#include<Arduino.h>
+
+enum { EV_NONE=0, EV_SHORTPRESS, EV_LONGPRESS };
+
+
+class ButtonHandler {
+  public:
+    // Constructor
+    ButtonHandler(int pin, int longpress_len=DEFAULT_LONGPRESS_LEN);
+
+    // Initialization done after construction, to permit static instances
+    void init();
+
+    // Handler, to be called in the loop()
+    int handle();
+
+  protected:
+    boolean was_pressed;     // previous state
+    int pressed_counter;     // press running duration
+    const int pin;           // pin to which button is connected
+    const int longpress_len; // longpress duration
+};
+
+ButtonHandler::ButtonHandler(int p, int lp)
+: pin(p), longpress_len(lp)
+{
+}
+
+void ButtonHandler::init()
+{
+  pinMode(pin, INPUT);
+  digitalWrite(pin, HIGH); // pull-up
+  was_pressed = false;
+  pressed_counter = 0;
+}
+
+int ButtonHandler::handle()
+{
+  int event;
+  int now_pressed = !digitalRead(pin);
+
+  if (!now_pressed && was_pressed) {
+    // handle release event
+    if (pressed_counter < longpress_len)
+      event = EV_SHORTPRESS;
+    else
+      event = EV_LONGPRESS;
+  }
+  else
+    event = EV_NONE;
+
+  // update press running duration
+  if (now_pressed)
+    ++pressed_counter;
+  else
+    pressed_counter = 0;
+
+  // remember state, and we're done
+  was_pressed = now_pressed;
+  return event;
+};
+
+#endif
